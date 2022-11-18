76aae92572c739c802612ef0230f76bc12e91db5
Tom <kouzhenju@163.com>
2022-11-18 08:14:03 +0800

    Add method isJsonElementEqualsWithSomeJsonArrayOrder and test case.

--- gson-compare/src/main/java/com/tom/util/GsonUtils.java
+++ gson-compare/src/main/java/com/tom/util/GsonUtils.java
@@ -5,11 +5,79 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class GsonUtils {
     private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+
+    public static boolean isJsonElementEqualsWithSomeJsonArrayOrder(String x, String y, List<String> keys) {
+        return isJsonElementEqualsWithSomeJsonArrayOrder(gson.fromJson(x,JsonElement.class),gson.fromJson(y,JsonElement.class),keys);
+    }
+
+    public static boolean isJsonElementEqualsWithSomeJsonArrayOrder(JsonElement x, JsonElement y, List<String> keys) {
+        if (x.isJsonNull() && y.isJsonNull()) {
+            return true;
+        } else if (x.isJsonArray()&&y.isJsonArray()) {
+            JsonArray xArray = x.getAsJsonArray();
+            JsonArray yArray = y.getAsJsonArray();
+
+            if (xArray.size()==yArray.size()) {
+                for (int i = 0; i < xArray.size(); i++) {
+                    boolean flag = false;
+
+                    for (int j = 0; j < yArray.size(); j++) {
+                        if (isJsonElementEqualsWithoutJsonArrayOrder(xArray.get(i),yArray.get(j))) {
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    if (!flag) {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+
+        }
+        else if (x.isJsonObject() && y.isJsonObject()) {
+            Set<Map.Entry<String, JsonElement>> xSet = x.getAsJsonObject().entrySet();
+            Set<Map.Entry<String, JsonElement>> ySet = y.getAsJsonObject().entrySet();
+
+            if (xSet.size()==ySet.size()) {
+                for (Map.Entry<String, JsonElement> xEntry : xSet) {
+                    boolean flag = false;
+
+                    for (Map.Entry<String, JsonElement> yEntry : ySet) {
+                        if (xEntry.getKey().equals(yEntry.getKey())&&!keys.contains(xEntry.getKey())){
+                            if (isJsonElementEqualsWithoutJsonArrayOrder(xEntry.getValue(),yEntry.getValue())) {
+                                flag = true;
+                                break;
+                            }
+                        } else if (xEntry.getKey().equals(yEntry.getKey())&&keys.contains(xEntry.getKey())) {
+                            flag = isJsonElementEqualsWithJsonArrayOrder(xEntry.getValue(),yEntry.getValue());
+                        }
+                    }
+
+                    if (!flag) {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        else if (x.isJsonPrimitive() && y.isJsonPrimitive()) {
+            return x.getAsJsonPrimitive().equals(y.getAsJsonPrimitive());
+        } else {
+            return false;
+        }
+
+        return true;
+    }
 
     public static boolean isJsonElementEqualsWithoutJsonArrayOrder(String x, String y) {
         return isJsonElementEqualsWithoutJsonArrayOrder(gson.fromJson(x,JsonElement.class), gson.fromJson(y,JsonElement.class));
--- gson-compare/src/test/java/com/tom/util/TestGsonUtils.java
+++ gson-compare/src/test/java/com/tom/util/TestGsonUtils.java
@@ -3,12 +3,16 @@ package com.tom.util;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static com.tom.util.GsonUtils.isJsonElementEqualsWithJsonArrayOrder;
-import static com.tom.util.GsonUtils.isJsonElementEqualsWithoutJsonArrayOrder;
+import java.util.Arrays;
+
+import static com.tom.util.GsonUtils.*;
 
 public class TestGsonUtils {
     String s1 = "{\"name\":\"Tom\", \"age\":18}";
     String s2 = "{\"name\":\"Tom\", \"age\":19}";
+
+    String order1 = "{\"name\":\"Tom\", \"age\":18, \"order\":[1,2,3], \"noOrder\":[1,2,3,4]}";
+    String order2 = "{\"name\":\"Tom\", \"age\":18, \"order\":[2,3,1], \"noOrder\":[2,3,4,1]}";
     @Test
     public void testIsJsonElementEqualsWithoutJsonArrayOrder(){
         Assert.assertTrue(isJsonElementEqualsWithoutJsonArrayOrder(s1,s1));
@@ -19,5 +23,12 @@ public class TestGsonUtils {
     public void testIsJsonElementEqualsWithJsonArrayOrder(){
         Assert.assertTrue(isJsonElementEqualsWithJsonArrayOrder(s1,s1));
         Assert.assertFalse(isJsonElementEqualsWithJsonArrayOrder(s1,s2));
+    }
+
+    @Test
+    public void testIsJsonElementEqualsWithSomeJsonArrayOrder(){
+        Assert.assertTrue(isJsonElementEqualsWithSomeJsonArrayOrder(order1, order1, Arrays.asList(new String[]{"order"})));
+        Assert.assertFalse(isJsonElementEqualsWithSomeJsonArrayOrder(order1, order2, Arrays.asList(new String[]{"order"})));
+
     }
 }
